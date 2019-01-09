d8ba1bfaca36c89155bfdcf91e878dd331c59d51
Saad Sharif <s1745977@sms.ed.ac.uk>
2019-01-09 03:24:36 +0100

    dark knight dummo unfinished tree

--- main.js
+++ main.js
@@ -1,8 +1,78 @@
-// window.addEventListener("DOMContentLoaded", setBodyGradientOnLoad);
 
-// function setBodyGradientOnLoad() {
-//     var heightPx = window.innerHeight;
-//     var gradientStop = Math.floor( heightPx * 0.3 );
-//     var body = document.getElementsByTagName("body")[0];
-//     body.style.backgroundImage = "linear-gradient(to bottom, ffd700, ffd700 " + gradientStop + "px, 222222 " + gradientStop + "px, 222222)";
+
+// DO NOT NEED, DOn't know what to do with tree shit for now 
+
+// function treeFunct(){
+//     var password = prompt("Password: ", "Enter Password")
+    
+//     if(password == "killbill"){
+//         txt =  "Correct";
+//         window.location.href = "projectTree/roots.html"; 
+//     }
+//     else if(password != "killbill"){
+//         txt = "Wrong Password."
+//         window.alert(txt);
+//     }
+//     else{
+//         txt = "error"
+//         window.alert(txt);
+//     }
 // }
+
+function goHome(){
+    window.location.href = "index.html"
+}
+
+var i = 0;
+var txt = 'SAAD SHARIF'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+function colourSwitch(){
+    // if (switch == 0){
+
+    // }else if(switch == 1){
+
+    // }
+    document.body.style.background = "#ffd700";
+    document.body.style.color = "#ffd700";
+    
+    
+    // document.getElementsByTagName("a").style.color = "#222222";
+    // var sel = document.getElementsByClassName(select);
+    // sel.style.color = "#222222";
+    
+    
+    // if(background-color == #ffd700){
+
+    // }else if (){
+
+    // }
+    var x = document.getElementByClassName("heading");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+
+
+        var x = document.getElementById("navlink");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    
+}
+
+function killbill(){
+    window.location.href = "kilbil.html";
+}
